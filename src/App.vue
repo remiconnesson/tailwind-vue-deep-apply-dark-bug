b5db7d13d4e51012c37ef8c4c64e1995c966b441
Rémi Connesson <remiconnesson2@gmail.com>
2023-04-17 17:03:31 +0200

    Demo bug

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
 import { useDark, useToggle } from "@vueuse/core";
+import ChildText from "@/components/ChildText.vue";
+
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <h1 class="text-green-700 dark:text-green-300">Hello</h1>
-  <button class="text-black dark:text-white rounded shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-slate-700 hover:bg-gray-100 dark:hover:bg-slate-950 p-4" @click="toggleDark()">
+  <button
+    class="text-black dark:text-white rounded shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-slate-700 hover:bg-gray-100 dark:hover:bg-black p-4"
+    @click="toggleDark()"
+  >
     Dark: {{ isDark }} , click to change
   </button>
   <span class="block classic-apply">this is the target of an @apply rule</span>
+  <div class="wrapper">
+    <ChildText />
+  </div>
 </template>
 
 <style scoped>
 .classic-apply {
-  @apply text-xl text-orange-700 dark:text-white
+  @apply text-xl text-orange-700 dark:text-white;
+}
+
+.wrapper :deep(.child-text) {
+  @apply text-pink-700 dark:text-yellow-300;
+}
+</style>
+
+<style>
+.wrapper .child-text-global {
+  @apply text-sky-700 dark:text-purple-300;
 }
 </style>
---
+++ src/components/ChildText.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="child-text text-xl mt-8">
+    I am targeted by my parent component with
+  </div>
+  <pre class="block child-pre text-black dark:text-white">
+&lt;style scoped&gt;
+.wrapper :deep(.child-text) {
+  @apply text-pink-700 dark:text-yellow-300
+}
+&lt;/style&gt;
+  </pre>
+
+  <div class="child-text-global text-xl mt-8">
+    I am targeted by my parent component with
+  </div>
+  <pre class="block child-pre text-black dark:text-white">
+&lt;style&gt;
+.child-text-global {
+  @apply text-sky-700 dark:text-purple-300
+}
+&lt;/style&gt;
+  </pre>
+</template>
+
+<style scoped></style>
