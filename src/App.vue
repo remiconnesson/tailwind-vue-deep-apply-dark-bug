904803cbd2ea173d89d1afeb401f8eef55cafb6d
Rémi Connesson <remiconnesson2@gmail.com>
2023-04-17 11:43:46 +0200

    Use apply for dark mode

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,14 @@ const toggleDark = useToggle(isDark);
 
 <template>
   <h1 class="text-green-700 dark:text-green-300">Hello</h1>
-  <button class="text-black dark:text-white" @click="toggleDark()">
-    Dark: {{ isDark }}
+  <button class="text-black dark:text-white rounded shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-slate-700 hover:bg-gray-100 dark:hover:bg-slate-950 p-4" @click="toggleDark()">
+    Dark: {{ isDark }} , click to change
   </button>
+  <span class="block classic-apply">this is the target of an @apply rule</span>
 </template>
+
+<style scoped>
+.classic-apply {
+  @apply text-xl text-orange-700 dark:text-white
+}
+</style>
