76dac3ba7dbfb65bae4b9cbfb0b39c087e2223ce
Rémi Connesson <remiconnesson2@gmail.com>
2023-04-17 11:38:13 +0200

    Install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core'
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+import { useDark, useToggle } from "@vueuse/core";
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <h1 class="text-green-700 dark:text-green-300">Hello</h1>
-  <button class="text-black dark:text-white" @click="toggleDark()">Dark: {{ isDark }}</button> 
+  <button class="text-black dark:text-white" @click="toggleDark()">
+    Dark: {{ isDark }}
+  </button>
 </template>
