c14185ff319803d4a6ad4710e6c895d7cfbfe8f4
Rémi Connesson <remiconnesson2@gmail.com>
2023-04-17 11:33:35 +0200

    Set up darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
 
 <template>
-  <h1 class="text-green-700">Hello</h1>
+  <h1 class="text-green-700 dark:text-green-300">Hello</h1>
+  <button class="text-black dark:text-white" @click="toggleDark()">Dark: {{ isDark }}</button> 
 </template>
